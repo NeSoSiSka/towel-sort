9946ae0b402af5bc246ee3e34a11380650d8e24e
NeSoSiSka <78197152+NeSoSiSka@users.noreply.github.com>
2021-03-14 21:50:33 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ module.exports = function towelSort (matrix) {
   }
   for (let i = 0; i < matrix.length; i++) {
     if(i%2 !== 0){
-      matrix[i].reverse();
+      matrix[i].reverse()
     } 
   }
   return matrix.flat()
